fab6380621eb5d2afa8dfa9f2b0e0747fb52b26c
mazenham1d <hamidm@msoe.edu>
2024-11-08 13:39:42 -0600

    Implemented some more methods

--- Network.cpp
+++ Network.cpp
@@ -5,33 +5,32 @@
 #include "Network.h"
 
 #include <iostream>
-#include <utility>
 
 void initializeNetwork(Network *network) {}
 
 // TODO: Parse layout string and initialize rooms based on it
 // Constructor to initialize the network and store layout
 Network::Network(std::string  layout) : layout(std::move(layout)) {
+    int index = 0;
     for (auto & room : rooms) {
         for (auto & j : room) {
-            j = new Computer();
+            char const icon = this->layout[index]; // Casting to char because it's a string
+            j = new Computer(icon);
+            j->setCompromised(false);
+            ++index;
         }
     }
 }
 
+
+
 // Overload the << operator to print the network map based on layout
 std::ostream& operator<<(std::ostream& os, const Network& network) {
     os << "Network Map:\n";
-    int index = 0;
 
-    for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            if (index < network.layout.length()) {
-                os << network.layout[index] << ' ';
-            } else {
-                os << "[ ] ";
-            }
-            ++index;
+    for (auto & room : network.rooms) {
+        for (auto & j : room) {
+            os << j->getIcon() << ' ';
         }
         os << '\n';
     }
--- Network.h
+++ Network.h
@@ -22,10 +22,14 @@ private:
 
 public:
     // Constructor to initialize the network
-    Network(std::string layout);
+    explicit Network(std::string layout);
 
-    // Method to print the map of the network
-    void map() const;
+    // Method to get the computer at a specific location
+    [[nodiscard]] Computer *getComputer(int row, int col) const {
+        return rooms[row][col];
+    }
+
+    // Method to scan adjacent computers for defenses
 
     // Overload the << operator to print the network
     friend std::ostream& operator<<(std::ostream& os, const Network& network);
@@ -35,29 +39,39 @@ class Computer {
 private:
     std::list<Defense *> defenses; // Vector of pointers to Defense objects
     std::vector<Attack*> loot;      // Vector of pointers to Attack objects
+
     bool compromised{};
+    char icon;
 
 public:
-    Computer() = default;
+    explicit Computer(const char icon) : icon(icon) {};
 
-    // Function to enter a room and check if it's compromised
-    bool enterRoom();
+    // Add/Set defense to the computer
+    void addDefense(Defense *defense) { defenses.push_back(defense); }
+
+    // Add/Set attack to the computer
+    void addAttack(Attack *attack) { loot.push_back(attack); }
+
+    // Get defenses
+    [[nodiscard]] std::list<Defense *> &get_defenses() { return defenses; }
+
+    // Get loot
+    [[nodiscard]] std::vector<Attack *> &get_loot() { return loot;};
+
+    // Get icon.
+    [[nodiscard]] char getIcon() const { return icon; }
 
     // Set the compromised state
-    void setCompromised(bool state) { compromised = state; }
+    void setCompromised(const bool state) { compromised = state; }
 
-    // Check if the computer is compromised
-    [[nodiscard]] bool isCompromised() const { return compromised; }
+    // Get the compromised state
+    [[nodiscard]] bool getCompromised() const { return compromised; }
 
     // Overload the << operator for Computer to show its status
     friend std::ostream& operator<<(std::ostream& os, const Computer& computer);
 
-    [[nodiscard]] std::list<Defense *> &get_defenses() {
-        return defenses;
-    }
 };
 
 std::ostream& operator<<(std::ostream& os, const Network& network);
 std::ostream& operator<<(std::ostream& os, const Computer& computer);
-
 #endif //NETWORK_H
