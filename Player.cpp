50f3f0ad0f9d433759bed0365eca39f4e4a6bec5
Tyler Schreiber <tylrschrbr@gmail.com>
2024-11-11 12:21:30 -0600

    check if attack has charges

--- Player.cpp
+++ Player.cpp
@@ -42,37 +42,70 @@ bool Player::doMove(const char command) {
 }
 
 void Player::aimAttack(char command, char direction) {
-    Direction attackDirection = {};
-    switch (direction) {
-        case 'N':
-            attackDirection = NORTH;
-        break;
-        case 'S':
-            attackDirection = SOUTH;
-        break;
-        case 'E':
-            attackDirection = EAST;
-        break;
-        case 'W':
-            attackDirection = WEST;
-        break;
-        default:
-            std::cerr << "Invalid input.";
-    }
-    if (Computer *targetComputer = currentRoom->getNeighbor(attackDirection);
-        targetComputer == nullptr) {
-        std::cerr << "No connection found to attack" << std::endl;
+    if (isAttackAvailable(command)) {
+        Direction attackDirection = {};
+        switch (direction) {
+            case 'N':
+                attackDirection = NORTH;
+            break;
+            case 'S':
+                attackDirection = SOUTH;
+            break;
+            case 'E':
+                attackDirection = EAST;
+            break;
+            case 'W':
+                attackDirection = WEST;
+            break;
+            default:
+                std::cerr << "Invalid input.";
+        }
+        if (Computer *targetComputer = currentRoom->getNeighbor(attackDirection);
+            targetComputer == nullptr) {
+            std::cerr << "No connection found to attack" << std::endl;
+            } else {
+                switch (command) {
+                    case Controller::BACKDOOR: {
+                        doAttack<Backdoor>(*targetComputer);
+                    }
+                    case Controller::CODE: {
+                        doAttack<IPSpoof>(*targetComputer);
+                    }
+                    case Controller::KEY: {
+                        doAttack<DatabaseEncryptionKey>(*targetComputer);
+                    }
+                }
+            }
     } else {
-        switch (command) {
-            case Controller::BACKDOOR: {
-                doAttack<Backdoor>(*targetComputer);
-            }
-            case Controller::CODE: {
-                doAttack<IPSpoof>(*targetComputer);
-            }
-            case Controller::KEY: {
-                doAttack<DatabaseEncryptionKey>(*targetComputer);
-            }
+        std::cerr << "Attack not available." << std::endl;
+    }
+}
+
+bool Player::isAttackAvailable(char attackType) {
+    Controller::ATTACKS type = {};
+    switch (attackType) {
+        case 'A':
+            type = Controller::CODE;
+        break;
+        case 'B':
+            type = Controller::BACKDOOR;
+        break;
+        case 'K':
+            type = Controller::KEY;
+        break;
+    }
+    for (const auto& attack : availableAttacks) {
+        switch (type) {
+            case Controller::CODE:
+                if (dynamic_cast<IPSpoof*>(attack)) return true;
+            break;
+            case Controller::BACKDOOR:
+                if (dynamic_cast<Backdoor*>(attack)) return true;
+            break;
+            case Controller::KEY:
+                if (dynamic_cast<DatabaseEncryptionKey*>(attack)) return true;
+            break;
         }
     }
+    return false;
 }
